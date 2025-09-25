ef9209bc9c846e0bad244b731a799cde80a12807
Hieu Le <166335731+HarrisonLe0211@users.noreply.github.com>
2025-09-24 23:22:41 -0700

    Update OTP-test.py

--- backend/auth_service/OTP-test.py
+++ backend/auth_service/OTP-test.py
@@ -72,7 +72,7 @@ def verify_otp(user_otp, generated_otp, start_time):
 if __name__ == "__main__":
     # Configuration
     sender_email = ""
-    sender_password = "bxsx lfyr kihn dpqs"  # Use an app password if necessary
+    sender_password = ""  # Use an app password if necessary
     recipient_email = input("Enter your email address: ")
 
     # Generate and send OTP
@@ -88,4 +88,5 @@ if __name__ == "__main__":
         print("Login successful!")
     else:
         print("Invalid OTP or OTP expired. Please try again.")
+
 
